e1075d8afef79a895f87f6b511a02536ba5d0da2
mmkamal021 <114946934+mmkamal021@users.noreply.github.com>
2024-03-03 17:08:54 +0600

    Get opposite number

---
+++ get-opposite.js
@@ -0,0 +1,10 @@
+// Give an integer or a floating-point Number,find its opposite.
+function getOpposite(number) {
+  return number * -1;
+}
+console.log(getOpposite(5));
+
+function getOpposite2(number) {
+  return -number;
+}
+console.log(getOpposite2(6.2));
